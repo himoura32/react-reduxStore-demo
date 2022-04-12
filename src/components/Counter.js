594f9b76d8ff1666a99dc4606c1f128826c95c32
himoura <fallingout31@gmail.com>
2022-04-12 11:45:18 -0400

    coded state slices using redux tk refactored

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,25 @@
 import { useSelector, useDispatch } from 'react-redux';
 
+import { counterActions } from '../store/counter';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
-  const show = useSelector(state => state.showCounter);
+  const counter = useSelector(state => state.counter.counter);
+  const show = useSelector(state => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: 'INCREMENT'})
+    dispatch(counterActions.increment())
   };
   const increaseHandler = () => {
-    dispatch({ type: 'INCREASE', amount: 5 })
+    dispatch(counterActions.increase(5))
   };
   const decrementHandler = () => {
-    dispatch({ type: 'DECREMENT'})
+    dispatch(counterActions.decrement())
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'TOGGLE'})
+    dispatch(counterActions.toggleCounter())
   };
 
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -1,62 +1,64 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
-const counterReducer = (state = initialState, action) => {
-
-  switch (action.type) {
-    case "INCREMENT":
-      return {
-        counter: state.counter + 1,
-        showCounter: state.showCounter,
-      };
-    case "INCREASE":
-      return {
-        counter: state.counter + action.amount,
-        showCounter: state.showCounter,
-      };
-    case "DECREMENT":
-      return {
-        counter: state.counter - 1,
-        showCounter: state.showCounter,
-      };
-    case "TOGGLE":
-      return {
-        showCounter: !state.showCounter,
-        counter: state.counter,
-      };
-    default:
-      return state;
-  }
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
 
 export default store;
 
+// if (action.type === 'INCREMENT') {
+//     return {
+//         counter: state.counter + 1,
+//         showCounter: state.showCounter
+//     };
+// }
+// if (action.type === 'INCREASE') {
+//     return {
+//         counter: state.counter + action.amount,
+//         showCounter: state.showCounter
+//     };
+// }
+// if (action.type === 'DECREMENT') {
+//     return {
+//         counter: state.counter -1,
+//         showCounter: state.showCounter
+//     };
+// }
+// if (action.type === 'TOGGLE') {
+//     return {
+//         showCounter: !state.showCounter,
+//         counter: state.counter
+//     }
+// };
+// return state;
 
-  // if (action.type === 'INCREMENT') {
-  //     return {
-  //         counter: state.counter + 1,
-  //         showCounter: state.showCounter
-  //     };
-  // }
-  // if (action.type === 'INCREASE') {
-  //     return {
-  //         counter: state.counter + action.amount,
-  //         showCounter: state.showCounter
-  //     };
-  // }
-  // if (action.type === 'DECREMENT') {
-  //     return {
-  //         counter: state.counter -1,
-  //         showCounter: state.showCounter
-  //     };
-  // }
-  // if (action.type === 'TOGGLE') {
-  //     return {
-  //         showCounter: !state.showCounter,
-  //         counter: state.counter
-  //     }
-  // };
-  // return state;
+// const counterReducer = (state = initialState, action) => {
+
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return {
+//         counter: state.counter + 1,
+//         showCounter: state.showCounter,
+//       };
+//     case "INCREASE":
+//       return {
+//         counter: state.counter + action.amount,
+//         showCounter: state.showCounter,
+//       };
+//     case "DECREMENT":
+//       return {
+//         counter: state.counter - 1,
+//         showCounter: state.showCounter,
+//       };
+//     case "TOGGLE":
+//       return {
+//         showCounter: !state.showCounter,
+//         counter: state.counter,
+//       };
+//     default:
+//       return state;
+//   }
+// };
